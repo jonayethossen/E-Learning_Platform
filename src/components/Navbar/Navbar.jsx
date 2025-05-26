c6aa7a85a3da84d859adf03d678cf3221f98736d
jonayethossen <jonayethossen8921@gmail.com>
2025-05-26 13:23:34 +0600

    nav running mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import { NavbarMenu } from "./../../MockData/data";
+import { IoMdMenu } from "react-icons/io";
+import Logo from "../../assets/logo.png";
+import NavPhoto from "../../assets/navPhoto.png";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-quaternary">
-        <div className="max-w-[1320px] px-16 py-8 flex justify-between items-center mx-auto">
+      <nav className="bg-quaternary border-b-1 border-white">
+        <div className=" container xl:max-w-[1320px] px-6  py-8 flex justify-between items-center mx-auto">
           {/* Logo section  */}
-          <img src="./Logo.png" alt="Logo" />
+          <img src={Logo} alt="Logo" />
           {/* Menu section  */}
           <div className="hidden md:block">
-            <ul className="flex items-center gap-12 text-white font-medium text-lg font-Montserrat ">
+            <ul className="flex items-center gap-12  ">
               {NavbarMenu.map((item) => {
                 return (
                   <li key={item.id}>
                     <a
                       href={item.link}
-                      className="inline-block px-3 py-1 hover:text-primary duration-200"
+                      className="inline-block px-3 py-1 text-white font-medium text-lg font-Montserrat  hover:text-primary duration-200"
                     >
                       {item.title}
                     </a>
                   </li>
                 );
               })}
-              <img src="./navPhoto.png" alt="" />
+              <img src={NavPhoto} alt="NavPhoto" />
             </ul>
           </div>
 
           {/* Mobile Humburger Menu section  */}
+          <div className="md:hidden">
+            <IoMdMenu className="text-4xl text-white" />
+          </div>
         </div>
       </nav>
       {/* Mobile slide ber section  */}
