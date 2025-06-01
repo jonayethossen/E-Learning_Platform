0a5d19781cbbe84fdd8c24b1f7a7875966f228f8
jonayethossen <jonayethossen8921@gmail.com>
2025-06-01 21:34:40 +0600

    hero part

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="bg bg-red-700">hero part </div>;
+  return (
+    <>
+      <section>
+        <div className="bg-tertiary  ">
+          <div className="container xl:max-w-[1320px] mx-auto px-6 py-20 ">
+            <div className="flex">
+              <h1 className="font-Montserrat text-white text-7xl font-bold max-w-[648px] h-[254px] leading-[86px]">
+                Learn New <br />
+                <span className="bg-primary py-0">Skills.</span> Build <br />
+                Your <span className="bg-secondary py-0"> Future.</span>
+              </h1>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
