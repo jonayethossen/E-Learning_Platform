b63430bc931f0dd5917453c9583b4dbb5897d5ea
jonayethossen <jonayethossen8921@gmail.com>
2025-06-12 10:20:59 +0600

    courses section

--- src/MockData/CoursesData.js
+++ src/MockData/CoursesData.js
@@ -1,44 +1,51 @@
+import Development_Icon from "../components/icons/Development";
+import Design_Icon from "../components/icons/Design";
+import Business_Icon from "../components/icons/Business";
+import Marketing_Icon from "../components/icons/Marketing";
+import Photography_Icon from "../components/icons/Photography";
+import Video_Icon from "../components/icons/Video";
+
 export const CoursesData = [
-    {
-        id:1,
-        title:"Development",
-        link:"#",
-        icon:"",
-        delay:0.2,
-    },
-    {
-        id:2,
-        title:"UI/UX Design",
-        link:"#",
-        icon:"",
-        delay:0.3,
-    },
-    {
-        id:3,
-        title:"Business",
-        link:"#",
-        icon:"",
-        delay:0.4,
-    },
-    {
-        id:4,
-        title:"Marketing",
-        link:"#",
-        icon:"",
-        delay:0.5,
-    },
-    {
-        id:5,
-        title:"Photography",
-        link:"#",
-        icon:"",
-        delay:0.6,
-    },
-    {
-        id:6,
-        title:"Video Editing",
-        link:"#",
-        icon:"",
-        delay:0.7,
-    }
+  {
+    id: 1,
+    title: "Development",
+    link: "#",
+    icon: Development_Icon,
+    delay: 0.2,
+  },
+  {
+    id: 2,
+    title: "UI/UX Design",
+    link: "#",
+    icon: Design_Icon,
+    delay: 0.3,
+  },
+  {
+    id: 3,
+    title: "Business",
+    link: "#",
+    icon: Business_Icon,
+    delay: 0.4,
+  },
+  {
+    id: 4,
+    title: "Marketing",
+    link: "#",
+    icon: Marketing_Icon,
+    delay: 0.5,
+  },
+  {
+    id: 5,
+    title: "Photography",
+    link: "#",
+    icon: Photography_Icon,
+    delay: 0.6,
+  },
+  {
+    id: 6,
+    title: "Video Editing",
+    link: "#",
+    icon: Video_Icon,
+    delay: 0.7,
+  },
 ];
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-// import { CoursesData } from "../../MockData/CoursesData";
+import { CoursesData } from "../../MockData/CoursesData";
 
 const Courses = () => {
   return (
     <section>
-    <div className="max-w-[1320px] px-6 mx-auto ">
-      <div className="grid-col-6"></div>
-    </div>
+      <div className="max-w-[1320px] px-6 mx-auto ">
+        <div className="grid grid-cols-6">
+          {CoursesData.map((courses) => (
+            <div key={courses.id}>
+              <courses.icon />
+              <h1>{courses.title} </h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
