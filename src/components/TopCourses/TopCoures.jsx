828311e38e64e9aa3b394c65013b89c41a3ad697
jonayethossen <jonayethossen8921@gmail.com>
2025-06-14 23:12:43 +0600

    top courses

--- src/components/TopCourses/TopCoures.jsx
+++ src/components/TopCourses/TopCoures.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const TopCoures = () => {
   return (
     <section className="bg-white min-h-[1410px] pt-[98px] ">
-      <div className=" bg-amber-300 containers ">jonayet</div>
+      <div className="containers ">
+        <div className="flex flex-col items-center justify-center text-center pt-[80px] gap-3">
+          <h1 className="text-5xl font-semibold font-Montserrat w-[666px] text-[#101010]">
+            Top Courses Recommended for You
+          </h1>
+          <h2 className="text-xl font-Roboto font-normal text-[#333333]">
+            Start learning with courses handpicked just for you — based on your
+            preferences and future ambitions.
+          </h2>
+        </div>
+      </div>
     </section>
   );
 };
